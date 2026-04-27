3ec97b280e63f7472ca1896089fd8a605267f11b
Mahpud || Nixx <bamat7010@gmail.com>
2026-04-27 19:46:17 +0700

    Delete api directory

--- api/all.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs")
-
-module.exports = (req, res) => {
-const db = JSON.parse(fs.readFileSync("./database/nomor.json"))
-res.setHeader("Content-Type", "application/json")
-res.end(JSON.stringify(db))
-}
